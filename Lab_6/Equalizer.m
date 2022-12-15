44b578d42530bcd9a3b67ea0edbc0dfc205e2f53
Artem Davydov <90029083+ArtemDavydov12@users.noreply.github.com>
2022-12-15 11:36:09 +0300

    Add files via upload

--- Lab_6/Equalizer.m
+++ Lab_6/Equalizer.m
@@ -1,28 +1,54 @@
 classdef Equalizer < handle
-    properties(Constant  =  true)
-         freqArray (10, 1) {double} = [31, 62, 125, 250, 500, 1000, 2000, 4000, 8000, 16000];
+    properties(Constant)
+        freqArray{mustBeReal} = [31, 62, 125, 250, 500, 1000, 2000, 4000, 8000,16000];
     end
-    properties (Access = public)
-        gain (10, 1) {double} = ones;
+    properties(Access = public)
+        gain = [1,1,1,1,1,1,1,1,1,1];
     end
-    properties (GetAccess = public, SetAccess = protected)
-        order (1, 1) {double} = 64;
-        fs (1, 1) {double} = 44100;
+    properties(GetAccess = public,SetAccess = protected)
+        order double {mustBeInteger} = 64;
+        fs double {mustBeReal} = 44100;
     end
     properties(Access = protected)
-        bBank {double};
-        initB {double} = [ ];
+        bBank{mustBeReal}=[];
+        initB{mustBeReal} = [];
     end
     methods
-        function obj = Equalizer(order, fs)
+        function obj = Equalizer(order,fs)
             obj.order = order;
             obj.fs = fs;
-            obj.bBank = zeros(length(freqArray), order + 1);
+            obj.CreateFilters();
         end
-        function obj = Filtering(gain, bBank, signal)
-            obj.bBank_new = sum(gain .* bBank);
-            obj.signalOut = filter(bBank_new, 1, signal);
+        function [signalOut,initB] = Filtering(obj,signal)
+            b = sum(obj.gain.*obj.bBank', 1);
+            [signalOut,initB] = filter(b, 1, signal, obj.initB);
         end
-    end
-end
-
+        function CreateFilters(obj)
+        freqArrayNorm = obj.freqArray/(obj.fs/2);
+        obj.bBank = zeros(length(obj.freqArray),obj.order+1);
+         for m = 1:length(obj.freqArray)
+            if m == 1
+                mLow = [1, 1, 0, 0];
+                freqLow = [0, freqArrayNorm(1), 2*freqArrayNorm(1), 1];
+                obj.bBank(1, :) = fir2(obj.order, freqLow, mLow);
+            elseif m == length(obj.freqArray)    
+                   mHigh = [0, 0, 1, 1];
+                   freqHigh = [0, freqArrayNorm(end)/2, freqArrayNorm(end), 1];
+                   obj.bBank(length(obj.freqArray), :) = fir2(obj.order, freqHigh, mHigh);
+            else 
+                mBand = [0, 0, 1, 0, 0];
+                freqBand = [0, freqArrayNorm(m-1), freqArrayNorm(m), freqArrayNorm(m+1), 1];
+                obj.bBank(m, :) = fir2(obj.order, freqBand, mBand);
+            end
+         end
+        end
+        function [HdB,w] = GetFreqResponce(obj)
+            b = sum(obj.gain.*obj.bBank',2);          
+            [H,w] = freqz(b,1,obj.order);
+            xdb = @(x)20*log10(x);
+            HdB = xdb(abs(H));
+            w = (w/pi)*(obj.fs/2);
+        end
+    end     
+end   
+        
